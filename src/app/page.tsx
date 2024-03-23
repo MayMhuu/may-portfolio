1f351d24c8fd3244bd604fb117adcd057f3eeb04
May Mhuu <maymhuu@Mays-MacBook-Pro.local>
2024-03-23 22:15:33 +0630

    remove uncessary file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import About from "@/components/home/about";
 import Services from "@/components/home/services";
 import Experiences from "@/components/home/experices";
 import Skills from "@/components/home/skills";
-import Projects from "@/components/home/projects";
 import Contact from "@/components/home/contact";
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
