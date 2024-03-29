55f1a47c1bef5874dd22bca4d50499c61df5b29e
May Mhuu <maymhuu@Mays-MacBook-Pro.local>
2024-03-29 20:31:07 +0630

    added png and fix css

--- src/components/projects/components/ScreenshotGallery.tsx
+++ src/components/projects/components/ScreenshotGallery.tsx
@@ -7,7 +7,6 @@ import Row from "@/components/layout/row";
 import Column from "@/components/layout/column";
 import { IScreenshotItem } from "@/interfaces";
 
-
 const ScreenshotGallery = ({
   imageList,
 }: Readonly<{ imageList: Array<IScreenshotItem> }>) => {
@@ -21,10 +20,14 @@ const ScreenshotGallery = ({
           return (
             <Link
               key={`screenshot-${index}`}
-              className="relative min-w-[12rem] lg:min-w-[14rem] max-w-[12rem] lg:max-w-[14rem] aspect-[9/16] border border-[var(--textColor10)] rounded-[var(--defaultRadius)] overflow-hidden drop_in"
+              className="hover:bg-zinc-600/10 hover:border-zinc-400/50 relative min-w-[12rem] lg:min-w-[14rem] max-w-[12rem] lg:max-w-[14rem] aspect-[9/16] border border-[var(--textColor10)] rounded-[var(--defaultRadius)] overflow-hidden drop_in"
               href={{
                 pathname: pathname,
-                query: { id: searchParams.get("id"), imgSrc: img.url,description:img.description },
+                query: {
+                  id: searchParams.get("id"),
+                  imgSrc: img.url,
+                  description: img.description,
+                },
               }}
               scroll={false}
             >
@@ -37,7 +40,7 @@ const ScreenshotGallery = ({
                 loading="lazy"
                 placeholder="blur"
                 blurDataURL="/images/placeholder.png"
-                className="w-full h-full object-cover py-4 px-2"
+                className="w-full h-full object-cover py-2 px-2 rounded-[var(--defaultRadius)]"
               />
             </Link>
           );
